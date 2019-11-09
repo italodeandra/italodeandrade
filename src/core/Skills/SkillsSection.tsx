c4f7f6f31e06cb396c36695580d3784d0fca8998
Ítalo Andrade <italodeandra@gmail.com>
2019-11-09 01:16:58 -0300

    Help me copy for reuse

--- src/core/Skills/SkillsSection.tsx
+++ src/core/Skills/SkillsSection.tsx
@@ -7,30 +7,30 @@ const SkillsSection: FunctionComponent = () => {
         <div id='skills' className='Skills Section' tabIndex={1}>
             <Text header>Skills</Text>
             <Text paragraph className='skills'>
-                <Text code className='skill'>JavaScript</Text>
-                <Text code className='skill'>ReactJS</Text>
-                <Text code className='skill'>Angular</Text>
-                <Text code className='skill'>HTML5</Text>
-                <Text code className='skill'>CSS3</Text>
-                <Text code className='skill'>SASS</Text>
-                <Text code className='skill'>UI/UX Design</Text>
-                <Text code className='skill'>PHP</Text>
+                <Text code select='all' className='skill'>JavaScript</Text>
+                <Text code select='all' className='skill'>ReactJS</Text>
+                <Text code select='all' className='skill'>Angular</Text>
+                <Text code select='all' className='skill'>HTML5</Text>
+                <Text code select='all' className='skill'>CSS3</Text>
+                <Text code select='all' className='skill'>SASS</Text>
+                <Text code select='all' className='skill'>UI/UX Design</Text>
+                <Text code select='all' className='skill'>PHP</Text>
             </Text>
             <Text paragraph className='skills'>
-                <Text code className='skill'>NodeJS</Text>
-                <Text code className='skill'>PostgreSQL</Text>
-                <Text code className='skill'>MongoDB</Text>
-                <Text code className='skill'>Java</Text>
-                <Text code className='skill'>MySQL</Text>
+                <Text code select='all' className='skill'>NodeJS</Text>
+                <Text code select='all' className='skill'>PostgreSQL</Text>
+                <Text code select='all' className='skill'>MongoDB</Text>
+                <Text code select='all' className='skill'>Java</Text>
+                <Text code select='all' className='skill'>MySQL</Text>
             </Text>
             <Text paragraph className='skills'>
-                <Text code className='skill'>Functional Programming</Text>
-                <Text code className='skill'>Object-Oriented Programming</Text>
-                <Text code className='skill'>AWS</Text>
-                <Text code className='skill'>Docker</Text>
-                <Text code className='skill'>GitLab CI</Text>
-                <Text code className='skill'>GitHub Actions</Text>
-                <Text code className='skill'>Agile Methodologies</Text>
+                <Text code select='all' className='skill'>Functional Programming</Text>
+                <Text code select='all' className='skill'>Object-Oriented Programming</Text>
+                <Text code select='all' className='skill'>AWS</Text>
+                <Text code select='all' className='skill'>Docker</Text>
+                <Text code select='all' className='skill'>GitLab CI</Text>
+                <Text code select='all' className='skill'>GitHub Actions</Text>
+                <Text code select='all' className='skill'>Agile Methodologies</Text>
             </Text>
         </div>
     )
