2ac9f4e8dd339831ffb2862e8f9cc4593d8d91ab
Ítalo Andrade <italodeandra@gmail.com>
2019-11-22 23:02:17 -0300

    Added more skills

--- src/core/Skills/SkillsSection.tsx
+++ src/core/Skills/SkillsSection.tsx
@@ -8,29 +8,33 @@ const SkillsSection: FunctionComponent = () => {
             <Text header>Skills</Text>
             <Text paragraph className='skills'>
                 <Text code select='all' className='skill'>JavaScript</Text>
+                <Text code select='all' className='skill'>TypeScript</Text>
                 <Text code select='all' className='skill'>ReactJS</Text>
                 <Text code select='all' className='skill'>Angular</Text>
+                <Text code select='all' className='skill'>AngularJS</Text>
                 <Text code select='all' className='skill'>HTML5</Text>
                 <Text code select='all' className='skill'>CSS3</Text>
                 <Text code select='all' className='skill'>SASS</Text>
                 <Text code select='all' className='skill'>UI/UX Design</Text>
+                <Text code select='all' className='skill'>jQuery</Text>
+            </Text>
+            <Text paragraph className='skills'>
+                <Text code select='all' className='skill'>Java (Spring Boot, JPA, JUnit)</Text>
+                <Text code select='all' className='skill'>NodeJS (Express/Hapi, TypeScript, Mocha)</Text>
+                <Text code select='all' className='skill'>PostgreSQL</Text>
+                <Text code select='all' className='skill'>MySQL</Text>
+                <Text code select='all' className='skill'>MongoDB</Text>
                 <Text code select='all' className='skill'>PHP</Text>
             </Text>
             <Text paragraph className='skills'>
-                <Text code select='all' className='skill'>NodeJS</Text>
-                <Text code select='all' className='skill'>PostgreSQL</Text>
-                <Text code select='all' className='skill'>MongoDB</Text>
-                <Text code select='all' className='skill'>Java</Text>
-                <Text code select='all' className='skill'>MySQL</Text>
-            </Text>
-            <Text paragraph className='skills'>
+                <Text code select='all' className='skill'>Agile Methodologies</Text>
                 <Text code select='all' className='skill'>Functional Programming</Text>
                 <Text code select='all' className='skill'>Object-Oriented Programming</Text>
                 <Text code select='all' className='skill'>AWS</Text>
                 <Text code select='all' className='skill'>Docker</Text>
                 <Text code select='all' className='skill'>GitLab CI</Text>
                 <Text code select='all' className='skill'>GitHub Actions</Text>
-                <Text code select='all' className='skill'>Agile Methodologies</Text>
+                <Text code select='all' className='skill'>Sonarqube</Text>
             </Text>
         </div>
     )
