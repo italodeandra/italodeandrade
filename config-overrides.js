9fdb10912903a37fdaf1694db5cec0a95d087e56
Ítalo Andrade <italodeandra@gmail.com>
2021-04-05 23:18:17 +0200

    change my website to something more simple and that shows the projects I'm proud of

--- config-overrides.js
+++
@@ -1,5 +0,0 @@
-const { useBabelRc, override, removeModuleScopePlugin } = require('customize-cra')
-
-module.exports = override(
-  useBabelRc(), removeModuleScopePlugin()
-)
---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import "../styles.css"
+import "@fontsource/inter/variable-full.css"
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+// noinspection JSUnusedGlobalSymbols
+export default MyApp
